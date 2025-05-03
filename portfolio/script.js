fcc538241558d40ac3e6767c8d4ad7357271881b
Simon212509 <147604254+Simon212509@users.noreply.github.com>
2025-05-03 05:03:30 +0100

    Add files via upload

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,10 +1,9 @@
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    const message = document.getElementById('form-message');
-    message.textContent = 'Thank you! Your message has been sent.';
-    message.style.color = 'green';
-    this.reset();
-  });
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  document.getElementById('form-message').textContent = "Thank you! Your message has been sent.";
+  this.reset();
+});
+
   
   
   
